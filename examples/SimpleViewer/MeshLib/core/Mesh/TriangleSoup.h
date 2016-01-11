52802bb003d829159ada31fc834027c011da8f0d
ptitSeb <sebastien.chev@gmail.com>
2016-01-11 19:53:17 +0100

    Added an example

---
+++ examples/SimpleViewer/MeshLib/core/Mesh/TriangleSoup.h
@@ -0,0 +1,540 @@
+#ifndef _TRIANGLE_SOUP_H_
+#define _TRIANGLE_SOUP_H_
+
+#include <vector>
+#include <map>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+
+#include "../Geometry/Point.h"
+#include "../Geometry/Point2.h"
+#include "../Parser/parser.h"
+#include "../Parser/StrUtil.h"
+
+namespace MeshLib
+{
+namespace TriangleSoup
+{
+
+class CVertex
+{
+	public:
+		CPoint    m_point;
+		CPoint2   m_uv;
+		CPoint    m_rgb;
+		CPoint    m_normal;
+		int       m_id;
+		int 	  m_cnt;
+		double    m_area;
+};
+
+class CFace
+{
+	public:
+		int m_id;
+		CVertex * m_v[3];
+		CPoint    m_normal;
+		double    m_area;
+};
+
+
+
+class CTriangleMesh
+{
+  public:
+
+	CTriangleMesh()
+	{
+		m_has_uv     = false;
+		m_has_rgb    = false;
+		m_has_normal = false;
+	};
+
+	~CTriangleMesh(){
+		for( size_t i = 0; i < m_vertices.size(); i ++ )
+		{
+			CVertex * pV = m_vertices[i];
+			delete pV;
+		}
+		for( size_t i = 0; i < m_faces.size(); i ++ )
+		{
+			CFace * pF = m_faces[i];
+			delete pF;
+		}
+		m_vertices.clear();
+		m_faces.clear();
+	};
+	
+	bool m_has_uv;
+	bool m_has_rgb;
+	bool m_has_normal;
+
+
+	std::vector<CFace*> 	& faces() { return	m_faces;};
+	std::vector<CVertex*> 	& vertices() { return	m_vertices;};
+
+   protected:
+
+	std::vector<CVertex*>   m_vertices;
+	std::vector<CFace*> 	m_faces;
+	std::map<int,CVertex*>  m_map;
+	// for obj reading
+	std::vector<CPoint>     m_points;
+	std::vector<CPoint2>    m_uvs;
+	std::vector<CPoint>     m_normals;
+	std::map<std::string,CVertex*> m_map2;
+
+
+   public:
+
+	void read_vertex_m( char * line )
+	{
+		char * str = strtok(line, " \r\n");
+		if( strcmp( str, "Vertex" ) != 0 )
+		{
+			std::cerr << "Error in reading vertex" << std::endl;
+			return;
+		}
+		CVertex * pV = new CVertex;
+		pV->m_cnt = m_vertices.size();
+        	pV->m_rgb = CPoint(1,1,1);
+
+		str = strtok(NULL, " \r\n");
+
+		pV->m_id = atoi( str );
+
+		CPoint p;
+		for( int i = 0; i < 3; i ++ )
+		{
+			str = strtok(NULL, " \r\n");
+			p[i] = atof( str );
+		}
+		pV->m_point = p;
+
+		str = strtok( NULL, "\r\n" );
+
+		if( str == NULL ) return;
+
+		std::string s(str);
+
+		size_t pos0 = s.find_first_of('{');
+		size_t pos1 = s.find_last_not_of('}');
+
+		s = s.substr( pos0+1, pos1);
+
+		CParser parser(s);
+
+		for( std::list<CToken*>::iterator iter = parser.tokens().begin(); iter != parser.tokens().end(); iter ++ )
+		{
+			CToken * pT = *iter;
+			if( pT->m_key == "rgb" )
+			{
+			   CPoint rgb;
+			   sscanf(pT->m_value.c_str(), "(%lf %lf %lf)", &rgb[0], &rgb[1], &rgb[2]);
+			   pV->m_rgb = rgb;
+			   m_has_rgb = true;
+			}
+			if( pT->m_key == "uv" )
+			{
+			   CPoint2 uv;
+			   sscanf(pT->m_value.c_str(), "(%lf %lf)", &uv[0], &uv[1]);
+			   pV->m_uv = uv;
+			   m_has_uv = true;
+			}
+		}
+
+		m_vertices.push_back( pV );
+		m_map[pV->m_id] = pV;
+	};
+
+	void read_face_m( char * line )
+	{
+		char * str = strtok(line, " \r\n");
+		if( strcmp( str, "Face" ) != 0 )
+		{
+			std::cerr << "Error in reading face" << std::endl;
+			return;
+		}
+		CFace * pF = new CFace;
+
+		str = strtok(NULL, " \r\n");
+		pF->m_id = atoi( str );
+
+		for( int i = 0; i < 3; i ++ )
+		{
+			str = strtok(NULL, " \r\n");
+			int id = atoi( str );
+			//pF->m_v[i] = m_vertices[id-1];
+			pF->m_v[i] = m_map[id];
+		}
+		m_faces.push_back( pF );
+	};
+
+	void read_m( const char * name )
+	{
+		FILE * fp=fopen(name,"r");
+		char line[1024];
+
+		while( !feof( fp ) )
+		{
+			if( fgets(line, 1024, fp) == NULL ) break;
+
+			if( strncmp( line, "Vertex", 6 ) == 0 )
+			{
+				read_vertex_m(line);
+				continue;
+			}
+			if( strncmp( line, "Face", 4 ) == 0 )
+			{
+				read_face_m(line);
+				continue;
+			}
+		}
+		fclose( fp );
+		m_map.clear();
+	}
+
+	void read_vertex_obj( char * line )
+	{
+		if( line[1]=='t' )
+		{
+			CPoint2 uv;
+			sscanf(line,"vt %lf %lf", &uv[0], &uv[1]);
+			m_uvs.push_back( uv );
+			return;
+		}
+		
+		if( line[1] == 'n')
+		{
+			CPoint n;
+			sscanf(line,"vn %lf %lf %lf", &n[0], &n[1], &n[2]);
+			m_normals.push_back( n );
+			return;
+		}
+
+		CPoint p;
+		sscanf(line,"v %lf %lf %lf", &p[0], &p[1], &p[2]);
+		m_points.push_back( p );
+	}
+	CVertex* add_vertex_obj(int point, int uv, int normal)
+	{
+		CVertex *c;
+		char buff[100];
+		sprintf(buff,"%d", point);
+		if (uv!=-1) sprintf(buff, "%s/%d", buff, uv);
+		if (normal!=-1) sprintf(buff, "%s/%d", buff, normal);
+		std::string s=buff;
+
+		std::map<std::string,CVertex*>::iterator search = m_map2.find(buff);
+		if (search==m_map2.end()) {
+			// create the vertex
+			c = new CVertex;
+			c->m_id = m_vertices.size();
+			c->m_cnt = m_vertices.size();
+			c->m_point = m_points[point-1];
+			if(uv!=-1) c->m_uv = m_uvs[uv-1];
+			if(normal!=-1) c->m_normal = m_normals[uv-1];
+			c->m_rgb = CPoint(1,1,1);
+			m_map2[s]=c;
+			m_vertices.push_back(c);
+		} else {
+			c = search->second;
+		}
+		return c;
+	}
+
+	void read_face_obj( char * line )
+	{
+		int faces[100][3];
+		int f = 0;
+		char* str = strtok(line, " \n");
+		while( str = strtok(NULL, " \n") )
+		{
+			faces[f][0]=-1; faces[f][1]=-1; faces[f][2]=-1;
+			char *str_uv=NULL, *str_norm=NULL;
+			str_uv=strchr(str,'/'); 
+			if(str_uv) {
+				str_uv[0]='\0'; str_uv++;
+				str_norm=strchr(str_uv,'/');
+				if(str_norm) {
+					str_norm[0]='\0'; str_norm++;
+				}
+			}
+			faces[f][0] = atoi(str);
+			if(str_uv && str_uv[0]!='\0') faces[f][1] = atoi(str_uv);
+			if(str_norm && str_norm[0]!='\0') faces[f][2] = atoi(str_norm);
+			f++;
+		}
+		if(f<2) return;
+		// add the faces and vertex
+		CVertex *c1 = add_vertex_obj(faces[0][0], faces[0][1], faces[0][2]);
+		CVertex *c2 = add_vertex_obj(faces[1][0], faces[1][1], faces[1][2]);
+		for (int i=2; i<f; i++) {
+			CVertex *c3 = add_vertex_obj(faces[i][0], faces[i][1], faces[i][2]);
+			CFace *f = new CFace;
+			f->m_id = m_faces.size();
+			f->m_v[0] = c1; f->m_v[1] = c2; f->m_v[2] = c3;
+			m_faces.push_back(f);
+			c2 = c3;
+		}
+		if (faces[0][1]!=-1) m_has_uv = true;
+		if (faces[0][2]!=-1) m_has_normal = true;
+	}
+
+	void read_obj( const char * name )
+	{
+
+		FILE * fp=fopen(name,"r");
+		char line[1024];
+
+		while( !feof( fp ) )
+		{
+			if( fgets(line, 1024, fp) == NULL ) break;
+			
+			if(line[0]=='v') 
+				read_vertex_obj(line);
+
+			if(line[0]=='f')
+				read_face_obj(line);
+		}
+		fclose( fp );
+		m_has_rgb = false;
+		m_points.clear();
+		m_uvs.clear();
+		m_normals.clear();
+		m_map.clear();
+	}
+};
+
+class CObjVertex
+{
+public:
+	int m_ipt;
+	int m_iuv;
+	int m_inl;
+};
+
+class CObjFace
+{
+public:
+	CObjVertex m_v[3];
+	double m_area;
+	CPoint m_normal;
+};
+
+class CObject
+{
+public:
+	CObject() 
+	{ 
+		m_has_uv = false;
+		m_has_normal = false;
+	}
+	
+	void read( const char * input );
+
+	std::vector<CPoint>  & points()  { return m_points;  };
+	std::vector<CPoint2> & uvs()     { return m_uvs;     };
+	std::vector<CPoint>  & normals() { return m_normals; };
+	std::vector<CObjFace> & faces()  { return m_faces;   };
+protected:
+
+	std::vector<CPoint>   m_points;
+	std::vector<CPoint2>  m_uvs;
+	std::vector<CPoint>   m_normals;
+
+	std::vector<CObjFace> m_faces;
+
+public:
+
+	bool m_has_uv;
+	bool m_has_normal;
+};
+
+void CObject::read( const char * input )
+{
+	FILE * fp;
+	
+	fp = fopen( input,"r" );
+
+	while( !feof(fp) )
+	{
+		char line[1024];
+		if( fgets(line,1024,fp) == NULL ) return;
+
+
+		if( line[0] == 'v' )
+		{
+
+			if( line[1]=='t' )
+			{
+				CPoint2 uv;
+				sscanf(line,"vt %lf %lf", &uv[0], &uv[1]);
+				m_uvs.push_back( uv );
+				continue;
+			}
+			
+			if( line[1] == 'n')
+			{
+				CPoint n;
+				sscanf(line,"vn %lf %lf %lf", &n[0], &n[1], &n[2]);
+				m_normals.push_back( n );
+				continue;
+			}
+
+			CPoint p;
+			sscanf(line,"v %lf %lf %lf", &p[0], &p[1], &p[2]);
+			m_points.push_back( p );
+			continue;
+		}
+
+		if( line[0] == 'f' )
+		{
+			CObjFace pf;
+
+			char* strcnt = strchr(line,' ');;
+			int c = 0;
+			while(strcnt && strcnt[0]!='\0') {
+				c++;
+				strcnt=strchr(strcnt+1,' ');				
+			}
+			
+			char * str = strtok( line, " \n" );
+
+			//count number of '/'
+			strcnt = strchr(line,'/');;
+			int cntslash = 0;
+			while(strcnt && strcnt[0]!='\0') {
+				cntslash++;
+				strcnt=strchr(strcnt+1,'/');				
+			}
+
+			int ids[50];
+			int count = 0;
+
+			while( str = strtok(NULL, " /\n") )
+			{
+				ids[count++] = atoi( str ) - 1;
+			}
+		
+			count /= cntslash;
+
+			for( int i = 0; i < 3 ; i ++ )
+			{
+				pf.m_v[i].m_ipt = ids[i*cntslash+0];
+			}
+
+			if( cntslash > 1 )
+			{
+				m_has_uv = true;
+				for( int i = 0; i < 3 ; i ++ )
+				{
+					pf.m_v[i].m_iuv = ids[i*cntslash+1];
+				}
+			}
+
+			if( cntslash == 3 )
+			{
+				m_has_normal = true;
+				for( int i = 0; i < 3 ; i ++ )
+				{
+					pf.m_v[i].m_inl = ids[i*cntslash+2];
+				}
+			}
+
+			m_faces.push_back(pf);
+		}		
+
+	}
+
+}
+
+
+}
+}
+#endif
+
+
+/* std::string version is slow
+
+void CObject::read( const char * input )
+{
+	std::ifstream file( input );
+
+	std::string line;
+
+	while( std::getline( file, line ) )
+	{
+		strutil::Tokenizer TK(line);
+
+		if( !TK.nextToken() ) continue;
+		std::string key = TK.getToken();
+
+		if( key == "v" )
+		{
+			CPoint p;
+			for(int i = 0; i < 3; i ++ )
+			{
+				TK.nextToken(" \n");
+				p[i] = strutil::parseString<double>( TK.getToken() );
+			}
+			m_points.push_back( p );
+			continue;
+		}
+
+		if( key == "vt" )
+		{
+			CPoint2 uv;
+			for(int i = 0; i < 2; i ++ )
+			{
+				TK.nextToken(" \n");
+				uv[i] = strutil::parseString<double>( TK.getToken() );
+			}
+			m_uvs.push_back( uv );
+			continue;
+		}
+
+		if( key == "vn" )
+		{
+			CPoint n;
+			for(int i = 0; i < 3; i ++ )
+			{
+				TK.nextToken(" \n");
+				n[i] = strutil::parseString<double>( TK.getToken() );
+			}
+			m_normals.push_back( n );
+			continue;
+		}
+
+		if( key == "f" )
+		{
+			CObjFace pf;
+			for( int i = 0; i < 3; i ++ )
+			{
+				TK.nextToken(" \n");			
+				strutil::Tokenizer STK( TK.getToken(), "/ \n" );
+
+				STK.nextToken();
+				pf.m_v[i].m_ipt = strutil::parseString<int>( STK.getToken() )-1;
+
+				if( STK.nextToken() )
+				{
+					pf.m_v[i].m_iuv = strutil::parseString<int>( STK.getToken() )-1;
+					m_has_uv = true;
+				}
+
+				if( STK.nextToken() )
+				{
+					pf.m_v[i].m_inl = strutil::parseString<int>( STK.getToken() )-1;
+					m_has_normal = true;
+				}
+			}
+			m_faces.push_back(pf);
+		}
+		
+
+	}
+
+}
+*/
---
+++ examples/SimpleViewer/Viewer/view_mesh.cpp
@@ -0,0 +1,631 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include<GL/glut.h>
+#include <gsalt/gsalt.h>
+#include "viewer/arcball.h"                           /*  Arc Ball  Interface         */
+#include "Mesh/TriangleSoup.h"
+#include "bmp/RgbImage.h"
+
+//compiling command
+//g++ -lGL -lglut -lGLU -IMeshLib view_mesh.C -o view_mesh
+
+using namespace MeshLib::TriangleSoup;
+using namespace MeshLib;
+
+
+CTriangleMesh mesh;
+
+/* window width and height */
+int win_width, win_height;
+int gButton;
+int startx,starty;
+int shadeFlag   = 0 ;
+
+/* rotation quaternion and translation vector for the object */
+MeshLib::CQrot       ObjRot(0,0,1,0);
+MeshLib::CPoint      ObjTrans(0,0,0);
+/* arcball object */
+MeshLib::CArcball arcball;
+
+/* geometry flag */
+int geometryFlag = 0;
+
+/* texture flag */
+int textureFlag =  2; 
+/* texture id and image */
+GLuint texName;
+RgbImage image;
+
+
+/*! setup the object, transform from the world to the object coordinate system */
+void setupObject(void)
+{
+    double rot[16];
+
+    glTranslated( ObjTrans[0], ObjTrans[1], ObjTrans[2]);
+    ObjRot.convert( rot );
+    glMultMatrixd(( GLdouble *)  rot );
+}
+
+/*! the eye is always fixed at world z = +5 */
+
+void setupEye(void){
+  glLoadIdentity();
+  gluLookAt( 0,0, 5,0,0,0,0,1,0);
+}
+
+/*! setup light */
+void setupLight()
+{
+	//MeshLib::CPoint position(0,0,-1);
+	MeshLib::CPoint position(0,0,1);
+	GLfloat lightOnePosition[4]={position[0], position[1], position[2], 0};
+	glLightfv(GL_LIGHT1, GL_POSITION, lightOnePosition);
+	MeshLib::CPoint position2(0,0,-1);
+	GLfloat lightTwoPosition[4]={position2[0], position2[1], position2[2], 0};
+	glLightfv(GL_LIGHT2, GL_POSITION, lightTwoPosition);
+}
+
+/*! draw mesh */
+void draw_mesh()
+{
+	  glBindTexture(GL_TEXTURE_2D, texName);
+
+	//glColor3f( 1.0, 1.0, 1.0 );
+	//glDisable( GL_LIGHTING );
+	for( size_t i = 0; i < mesh.faces().size(); i ++ )
+	{
+		CFace * pF = mesh.faces()[i];
+  		glBegin(GL_TRIANGLES);
+		if( shadeFlag == 0 )
+		{
+			glNormal3f( pF->m_normal[0], pF->m_normal[1], pF->m_normal[2] );
+			//glNormal3f( -pF->m_normal[0], -pF->m_normal[1], -pF->m_normal[2] );
+			//std::cout << pF->m_normal[0] << " " << pF->m_normal[1] << " " << pF->m_normal[2] << std::endl;
+		}
+		for( int j = 0; j < 3; j ++ )
+		{
+			CVertex * pV = pF->m_v[j];
+			if( shadeFlag == 1 )
+				glNormal3f( pV->m_normal[0], pV->m_normal[1], pV->m_normal[2] );
+
+	
+			if( mesh.m_has_uv )
+			{
+				MeshLib::CPoint2 uv = pV->m_uv;
+				glTexCoord2d( uv[0], uv[1] );
+			}
+
+			MeshLib::CPoint skin_rgb( 235/255.0,180/255.0,173/255.0);
+
+			if( mesh.m_has_rgb )
+				glColor3f( pV->m_rgb[0], pV->m_rgb[1], pV->m_rgb[2] );
+			else
+				//glColor3f( 1,1,1 );
+				glColor3f( skin_rgb[0], skin_rgb[1],skin_rgb[2] );
+
+			if( geometryFlag && mesh.m_has_uv )
+			{
+				glVertex3d( pV->m_uv[0], pV->m_uv[1] , 0);
+			}
+			else
+			{
+				glVertex3d( pV->m_point[0], pV->m_point[1], pV->m_point[2] );
+			}
+		}
+		glEnd();
+	}
+
+}
+
+
+/*! display call back function
+*/
+void display()
+{
+	/* clear frame buffer */
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	setupLight();
+	/* transform from the eye coordinate system to the world system */
+	setupEye();
+	glPushMatrix();
+	/* transform from the world to the ojbect coordinate system */
+	setupObject();
+  	/* draw the mesh */
+	draw_mesh();
+
+	glPopMatrix();
+	glutSwapBuffers();
+}
+
+/*! Called when a "resize" event is received by the window. */
+
+void reshape(int w, int h)
+{
+  float ar;
+
+  win_width=w;
+  win_height=h;
+
+  ar = (float)(w)/h;
+  glViewport(0, 0, w, h);               /* Set Viewport */
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+
+  // magic imageing commands
+  gluPerspective( 40.0, /* field of view in degrees */
+		  ar, /* aspect ratio */
+		  1.0, /* Z near */
+		  100.0 /* Z far */);
+
+  glMatrixMode(GL_MODELVIEW);
+
+  glutPostRedisplay();
+}
+
+/*! helper function to remind the user about commands, hot keys */
+void help()
+{
+  printf("w - Wireframe Display\n");
+  
+  printf("f  -  Flat Shading \n");
+  printf("s  -  Smooth Shading\n");
+  printf("t  -  Texture Mapping\n");
+  printf("r  -  Reset the view\n" );
+  printf("g  -  Toggle Geometry and UV view\n");
+
+  printf("o  -  Save frame buffer to snap_n.bmp\n");
+  printf("? -  Help Information\n");
+  printf("esc - quit\n");
+}
+
+/*! Keyboard call back function */
+
+void keyBoard(unsigned char key, int x, int y)
+{
+
+	switch( key )
+	{
+	case 'f':
+		//Flat Shading
+		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+		shadeFlag = 0;
+		break;
+	case 's':
+    		//Smooth Shading
+		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+		shadeFlag = 1;
+		break;
+	case 'w':
+	  	//Wireframe mode
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+		break;
+
+  case 't':
+    textureFlag = (textureFlag +1 )%3;
+    switch( textureFlag )
+    {
+    case 0:
+        glDisable(GL_TEXTURE_2D);
+        break;
+    case 1:
+        glEnable(GL_TEXTURE_2D);
+		    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
+        break;
+    case 2:
+       glEnable(GL_TEXTURE_2D);
+		    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
+       break;
+    }
+    break;
+
+  case 'g':
+	  geometryFlag = (geometryFlag + 1)%2;
+	  break;
+
+  case 'r':
+	  	ObjRot = MeshLib::CQrot(0,0,1,0);
+		ObjTrans = MeshLib::CPoint(0,0,0);
+	  break;
+
+  case '?':
+    help();
+    break;
+
+
+
+  case 27:
+		exit(0);
+		break;
+	}
+  glutPostRedisplay();
+}
+
+/*! setup GL states */
+void setupGLstate(){
+
+  GLfloat lightOneColor[] = {1, 1, 1, 1};
+  GLfloat globalAmb[] = {.1, .1, .1, 1};
+  GLfloat lightOnePosition[] = {.0,  .0, 1, 0.0};
+
+  glEnable(GL_CULL_FACE);
+  glFrontFace(GL_CCW);
+  //glFrontFace(GL_CW);
+  glEnable(GL_DEPTH_TEST);
+  //glClearColor(0,0,0,0);
+  glClearColor(1.0,1.0,1.0,1.0);
+  glShadeModel(GL_SMOOTH);
+
+
+  glEnable(GL_LIGHT1);
+  glEnable(GL_LIGHT2);
+  glEnable(GL_LIGHTING);
+  glEnable(GL_NORMALIZE);
+  glEnable(GL_COLOR_MATERIAL);
+
+  glLightfv(GL_LIGHT1, GL_DIFFUSE, lightOneColor);
+  glLightfv(GL_LIGHT2, GL_DIFFUSE, lightOneColor);
+  glLightModelfv(GL_LIGHT_MODEL_AMBIENT, globalAmb);
+  glColorMaterial(GL_FRONT, GL_AMBIENT_AND_DIFFUSE);
+
+  //glColorMaterial(GL_BACK, GL_AMBIENT_AND_DIFFUSE);
+
+  glLightfv(GL_LIGHT1, GL_POSITION, lightOnePosition);
+}
+
+/*! mouse click call back function */
+void  mouseClick(int button , int state, int x, int y){
+
+
+  /* set up an arcball around the Eye's center
+	  switch y coordinates to right handed system  */
+
+  if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
+  {
+      gButton = GLUT_LEFT_BUTTON;
+	  arcball = MeshLib::CArcball( win_width, win_height,  x-win_width/2,  win_height-y-win_height/2);
+  }
+
+  if (button == GLUT_MIDDLE_BUTTON && state == GLUT_DOWN) {
+      startx = x;
+      starty = y;
+      gButton = GLUT_MIDDLE_BUTTON;
+   }
+
+  if (button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN) {
+      startx = x;
+      starty = y;
+      gButton = GLUT_RIGHT_BUTTON;
+   }
+  return ;
+}
+
+/*! mouse motion call back function */
+
+void mouseMove(int x, int y)
+{
+	MeshLib::CPoint trans;
+	MeshLib::CQrot       rot;
+
+  /* rotation, call arcball */
+  if (gButton == GLUT_LEFT_BUTTON )
+  {
+      rot = arcball.update( x-win_width/2, win_height-y-win_height/2);
+      ObjRot =  rot * ObjRot;
+      glutPostRedisplay();
+  }
+
+  /*xy translation */
+  if (gButton == GLUT_MIDDLE_BUTTON)
+  {
+	  double scale = 10./win_height;
+      trans =  MeshLib::CPoint(scale*(x-startx), scale*(starty-y), 0  );
+	    startx = x;
+	    starty = y;
+      ObjTrans = ObjTrans + trans;
+      glutPostRedisplay();
+  }
+
+  /* zoom in and out */
+  if (gButton == GLUT_RIGHT_BUTTON ) {
+      double scale = 10./win_height;
+      trans =  MeshLib::CPoint(0,0, scale*(starty-y)   );
+	    startx = x;
+	    starty = y;
+      ObjTrans = ObjTrans+trans;
+      glutPostRedisplay();
+  }
+
+}
+
+
+void normalize( CTriangleMesh & mesh )
+{
+	MeshLib::CPoint center(0,0,0);
+	for( size_t i = 0; i < mesh.vertices().size(); i ++ )
+	{
+		CVertex * pV = mesh.vertices()[i];
+		center += pV->m_point;
+	}
+	center /= (double)(mesh.vertices().size());
+
+	for( size_t i = 0; i < mesh.vertices().size(); i ++ )
+	{
+		CVertex * pV = mesh.vertices()[i];
+		pV->m_point -= center;
+	}
+
+	double len = -1;
+	for( size_t i = 0; i < mesh.vertices().size(); i ++ )
+	{
+		CVertex * pV = mesh.vertices()[i];
+		for( int j = 0; j < 3; j ++ )
+		{
+			len = (len > fabs(pV->m_point[j]))? len: fabs(pV->m_point[j]);
+		}
+	}
+
+	for( size_t i = 0; i < mesh.vertices().size(); i ++ )
+	{
+		CVertex * pV = mesh.vertices()[i];
+		pV->m_point /= len;
+	}
+
+}
+
+void compute_normal( CTriangleMesh & mesh )
+{
+	for( size_t i = 0; i < mesh.faces().size(); i ++ )
+	{
+		CFace * pF = mesh.faces()[i];
+		MeshLib::CPoint p[3];
+		for( int j = 0; j < 3; j ++ )
+		{
+			p[j] = pF->m_v[j]->m_point;
+		}
+		MeshLib::CPoint nor = (p[1]-p[0])^(p[2]-p[0]);
+		pF->m_area = nor.norm();
+
+		nor /= nor.norm();
+		pF->m_normal = nor;
+	}
+
+	for( size_t i = 0; i < mesh.faces().size(); i ++ )
+	{
+		CFace * pF = mesh.faces()[i];
+		MeshLib::CPoint p[3];
+		for( int j = 0; j < 3; j ++ )
+		{
+			p[j] = pF->m_v[j]->m_point;
+		}
+		MeshLib::CPoint nor = (p[1]-p[0])^(p[2]-p[0]);
+		nor /= nor.norm();
+		pF->m_normal = nor;
+
+		for( int j = 0; j < 3; j ++ )
+		{
+			pF->m_v[j]->m_normal += pF->m_normal * pF->m_area;
+		}
+	}
+
+	for( size_t i = 0; i < mesh.vertices().size(); i ++ )
+	{
+		CVertex * pV = mesh.vertices()[i];
+		pV->m_normal /= pV->m_normal.norm();
+	}
+
+    for( size_t i = 0; i < mesh.vertices().size(); i ++ )
+	{
+		CVertex * pV = mesh.vertices()[i];
+		pV->m_area = 0;
+	}
+
+    //compute the vertex area
+	for( size_t i = 0; i < mesh.faces().size(); i ++ )
+	{
+		CFace * pF = mesh.faces()[i];
+		for( int j = 0; j < 3; j ++ )
+		{
+			pF->m_v[j]->m_area += pF->m_area/3.0;
+		}
+	}
+}
+
+/*! initialize bitmap image texture */
+
+void initialize_bmp_texture()
+{
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+	glGenTextures(1, &texName);
+	glBindTexture(GL_TEXTURE_2D, texName);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,   GL_LINEAR);
+
+	int ImageWidth  = image.GetNumCols();
+	int ImageHeight = image.GetNumRows();
+	GLubyte * ptr   = (GLubyte * )image.ImageData();
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 
+				        ImageWidth,
+						ImageHeight, 
+				        0, 
+				        GL_RGB, 
+				        GL_UNSIGNED_BYTE,
+						ptr);
+
+    if(textureFlag == 1)
+		  glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
+    else if(textureFlag == 2)
+		  glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
+    glEnable(GL_TEXTURE_2D);
+}
+
+
+/*! main function for viewer
+*/
+int main( int argc, char * argv[] )
+{
+	int reduce = 0;
+
+	if(argc==1) {
+		printf("Usage %s SampleFile [Texture] [-0..-100]  (to have -x%% of the triangles)\n", argv[0]);
+		exit(0);
+	}
+
+	std::string name( argv[1] );
+
+	unsigned pos = name.find_last_of(".");
+	std::string type  = name.substr( pos +1 );
+
+	if( type == "obj" )
+	{
+
+		mesh.read_obj( argv[1] );
+		normalize( mesh );
+		compute_normal( mesh );
+
+	}
+	else if( type == "m" )
+	{
+		mesh.read_m( argv[1] );
+		normalize( mesh );
+		compute_normal( mesh );
+	}
+
+	textureFlag = 0;
+
+	if( argc > 2 )
+	{
+		if( argv[2][0]!='-' ) 
+		{
+			image.LoadBmpFile( argv[2] );
+			textureFlag = 2;
+		}
+		else
+		{
+			reduce = atoi(argv[2]);
+		}
+		if( argc > 3 )
+		{
+			if( argv[3][0]!='-' )
+			{
+				image.LoadBmpFile( argv[3] );
+				textureFlag = 2;
+			}
+			else
+			{
+				reduce = atoi(argv[3]);
+			}
+		}
+	}
+	if (reduce>0) reduce = 0;
+	if(reduce<-100) reduce = -100;
+	if(reduce<0) {
+		int num_vertex=0, num_triangles=0;
+		bool with_uv, with_normal, with_color;
+		num_vertex = mesh.vertices().size();
+		num_triangles = mesh.faces().size();
+		with_uv = mesh.m_has_uv;
+		with_normal = mesh.m_has_normal;
+		with_color = mesh.m_has_rgb;
+
+		printf("Reducing geometry (%s uv, %s normals and %s colors) from %d triangles (%d vertices) to %d\n", (with_uv)?"with":"w/o",(with_normal)?"with":"w/o",(with_color)?"with":"w/o",num_triangles, num_vertex, (100+reduce)*num_triangles/100);
+		unsigned int flags = GSALT_FACE | GSALT_VERTEX;
+		if(with_uv) flags |= GSALT_TEXCOORD;
+		if(with_normal) flags |= GSALT_NORMAL;
+		if(with_color) flags |= GSALT_COLOR;
+		GSalt gsalt = gsalt_new(num_vertex, num_triangles, flags);
+
+		for (int i=0; i<num_vertex; i++) {
+			const CVertex *v = mesh.vertices()[i];
+			gsalt_add_vertex(gsalt, v->m_point[0], v->m_point[1], v->m_point[2]);
+			if(with_uv) gsalt_add_texcoord(gsalt, v->m_uv[0], v->m_uv[1]);
+			if(with_normal) gsalt_add_normal(gsalt, v->m_normal[0], v->m_normal[1], v->m_normal[2]);
+			if(with_color) gsalt_add_color(gsalt, v->m_rgb[0], v->m_rgb[1], v->m_rgb[2]);
+		}
+		for (int i=0; i<num_triangles; i++) {
+			const CFace *f = mesh.faces()[i];
+			gsalt_add_triangle(gsalt, f->m_v[0]->m_cnt, f->m_v[1]->m_cnt, f->m_v[2]->m_cnt);
+		}
+
+		int ret = gsalt_simplify(gsalt, (100+reduce)*num_triangles/100);
+
+		//put the new geometry
+		int new_vertex = gsalt_query_numvertex(gsalt);
+		int new_triangles = gsalt_query_numtriangles(gsalt);
+		if((new_vertex<num_vertex) && (new_triangles<num_triangles))
+		{
+			mesh.vertices().clear();
+			mesh.faces().clear();
+			for (int i=0; i<new_vertex; i++) {
+				float x, y, z;
+				CVertex *v = new CVertex;
+				v->m_area = 0.0;
+				v->m_id = i;
+				v->m_cnt = i;
+				gsalt_query_vertex(gsalt, i, &x, &y, &z);
+				v->m_point = CPoint(x,y,z);
+				if(with_uv) {
+					gsalt_query_texcoord(gsalt, i, &x, &y);
+					v->m_uv = CPoint2(x,y);
+				}
+				if(with_color) {
+					gsalt_query_color(gsalt, i, &x, &y, &z);
+				} else {
+					x=1.0f; y=1.0f; z=1.0f;
+				}
+				v->m_rgb = CPoint(x,y,z);
+				if(with_normal) {
+					gsalt_query_normal(gsalt, i, &x, &y, &z);
+					v->m_normal = CPoint(x,y,z);
+				}
+				mesh.vertices().push_back(v);
+			}
+			for (int i=0; i<new_triangles; i++) {
+				uint32_t a, b, c;
+				CFace *f = new CFace;
+				gsalt_query_triangle_uint32(gsalt, i, &a, &b, &c);
+				f->m_id = i;
+				f->m_area = 0.0;
+				f->m_v[0] = mesh.vertices()[a];
+				f->m_v[1] = mesh.vertices()[b];
+				f->m_v[2] = mesh.vertices()[c];
+				mesh.faces().push_back(f);
+			}
+			compute_normal( mesh );
+		}
+
+		gsalt_delete(gsalt);
+	} else
+		printf("Geometry (%s uv, %s normals and %s colors) is %d triangles (%d vertices) to %d\n", (mesh.m_has_uv)?"with":"w/o",(mesh.m_has_normal)?"with":"w/o",(mesh.m_has_rgb)?"with":"w/o",mesh.faces().size(), mesh.vertices().size());
+
+	/* glut stuff */
+	glutInit(&argc, argv);                /* Initialize GLUT */
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH );
+	glutInitWindowSize(600, 600);
+	glutCreateWindow("Mesh Viewer");        /* Create window with given title */
+	glViewport(0,0,800,800 );
+
+	glutDisplayFunc(display);             /* Set-up callback functions */
+	glutReshapeFunc(reshape);
+	glutMouseFunc(mouseClick);
+	glutMotionFunc(mouseMove);
+	glutKeyboardFunc(keyBoard);
+	setupGLstate();
+
+
+	if( textureFlag )
+	{
+		initialize_bmp_texture();
+
+	}
+
+	glutMainLoop();                       /* Start GLUT event-processing loop */
+
+	return 0;
+}
+
+
